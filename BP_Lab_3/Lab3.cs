c59b214fa7d32ceb8e42ebac981371bf332b2112
bobr_zlober <bobr.zlober@gmail.com>
2026-03-20 12:40:00 +0200

    finished third lab

--- BP_Lab_3/Lab3.cs
+++ BP_Lab_3/Lab3.cs
@@ -1,39 +1,79 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-public class Lab3
+public enum EvictionPolicy
 {
-    Dictionary<int, (int result, DateTime LastUsed)> cache = new Dictionary<int, (int result, DateTime LastUsed)>();
+    LRU,
+    LFU,
+    TTL,
+    Custom,
+}
+public class Lab3<TKey, TValue> where TKey : notnull
+{
+    Dictionary<TKey, (TValue result, DateTime LastUsed, int hits)> cache = new Dictionary<TKey, (TValue result, DateTime LastUsed, int hits)>();
+    Func<IEnumerable<Tkey>, Tkey>? _customEviction;
+    Func<TKey, TValue>_fn;
+    EvictionPolicy _policy;
+    public Lab3
+        (
+        Func<TKey, TValue>fn, 
+        EvictionPolicy policy,
+        Func<IEnumerable<TKey>, TKey>? customEviction
+        )
+    {
+        _policy = policy;
+        _fn = fn;
+        _customEviction = customEviction;
+    }
 
-    public int Square(int n)
+    public TValue Get(TKey n)
     {
         if (cache.ContainsKey(n))
         {
-            Console.WriteLine($"беру з кешу!");
-            cache[n] = (cache[n].result, DateTime.Now);
-            return cache[n].result;
+            if(_policy == EvictionPolicy.TTL)
+            {
+                var age = DateTime.Now - cache[n].LastUsed;
+                if(age.TotalSeconds >= 5)
+                {
+                    cache.Remove(n);
+                    Console.WriteLine($"{n} removed from the cache due to expiration");
+                }
+            }
+            if(cache.ContainsKey(n))
+            {
+                Console.WriteLine($"taking {n} from cache");
+                cache[n] = (cache[n].result, DateTime.Now, cache[n].hits ++);
+                return cache[n].result; 
+            }
+
         }
 
-        Console.WriteLine($"calculating {n} * {n}");
-        int result = n * n;
+        Console.WriteLine($"calculating {fn}");
+        TValue result = _fn(n);
 
         if (cache.Count >= 3)
         {
-            var oldest = cache.OrderBy(x => x.Value.LastUsed).First().Key;
-            cache.Remove(oldest);
-            Console.WriteLine($"{oldest} has been removed from cache");
+            TKey keyToRemove;
+            if(_policy == EvictionPolicy.LRU)
+            {
+                keyToRemove = cache.OrderBy(x => x.Value.LastUsed). First().Key;
+            } 
+            else if(_policy == EvictionPolicy.LFU)
+            {
+                keyToRemove = cache.OrderBy(x => x.Value.hits). First().Key;
+            } 
+            else if (_policy == EvictionPolicy.Custom)
+            {
+                keyToRemove = _customEviction!(cache.Keys);
+            } else
+            {
+                keyToRemove = cache.Keys.First();
+            }
+            cache.Remove(keyToRemove);
+            Console.WriteLine($"{n} was removed from the cache");
         }
 
-        cache[n] = (result, DateTime.Now);
+        cache[n] = (result, DateTime.Now, 0);
         return result;
     }
-    static void Test()
-    {
-        var p = new Lab3();
-        
-        
-    }
-    
 }
