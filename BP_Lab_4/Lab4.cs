5fd2eee2ede7a4c2f82ce02135bdb33990f10ac6
bobr_zlober <bobr.zlober@gmail.com>
2026-03-24 04:17:00 +0200

    add Node class and queue structure

---
+++ BP_Lab_4/Lab4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Lab4;
+
+class Node
+{
+    public double Value;
+    public int Priority;
+    public int InsertionOrder;
+    public Node Next;
+    public Node Prev;
+}
+
+public enum DequeueMode
+{
+    Highest,
+    Lowest,
+    Oldest,
+    Newest,
+}
+
+class BiDirectionalPriorityQueue
+{
+    Node head = null;
+    Node tail = null;
+    int insertionCount = 0;
+}
