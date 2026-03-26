3817bcc2281ad2cc540d183918228175f20b7108
bobr_zlober <bobr.zlober@gmail.com>
2026-03-26 18:43:00 +0200

    add Enqueue and RemoveNode

--- BP_Lab_4/Lab4.cs
+++ BP_Lab_4/Lab4.cs
@@ -23,4 +23,48 @@ class BiDirectionalPriorityQueue
     Node head = null;
     Node tail = null;
     int insertionCount = 0;
+
+    public void Enqueue(double value, int priority)
+    {
+        Node newNode = new Node();
+        newNode.Value = value;
+        newNode.Priority = priority;
+        newNode.InsertionOrder = insertionCount;
+        insertionCount++;
+        newNode.Next = null;
+        newNode.Prev = null;
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.Next = newNode;
+            newNode.Prev = tail;
+            tail = newNode;
+        }
+    }
+
+    private void RemoveNode(Node target)
+    {
+        if (target == head)
+        {
+            head = target.Next;
+            if (head != null)
+                head.Prev = null;
+            else
+                tail = null;
+        }
+        else if (target == tail)
+        {
+            tail = target.Prev;
+            tail.Next = null;
+        }
+        else
+        {
+            target.Prev.Next = target.Next;
+            target.Next.Prev = target.Prev;
+        }
+    }
 }
