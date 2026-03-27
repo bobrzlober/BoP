50f916030a6fa0c6e67bd6661d36ba346db4db8a
bobr_zlober <bobr.zlober@gmail.com>
2026-03-27 19:52:00 +0200

    fix InsertionOrder counter, add Dequeue

--- BP_Lab_4/Lab4.cs
+++ BP_Lab_4/Lab4.cs
@@ -26,11 +26,11 @@ class BiDirectionalPriorityQueue
 
     public void Enqueue(double value, int priority)
     {
+        insertionCount++;
         Node newNode = new Node();
         newNode.Value = value;
         newNode.Priority = priority;
         newNode.InsertionOrder = insertionCount;
-        insertionCount++;
         newNode.Next = null;
         newNode.Prev = null;
         if (head == null)
@@ -66,5 +66,67 @@ class BiDirectionalPriorityQueue
             target.Prev.Next = target.Next;
             target.Next.Prev = target.Prev;
         }
+    }
+
+    public Node Dequeue(DequeueMode mode)
+    {
+        if (head == null)
+        {
+            Console.WriteLine("the List is empty");
+            return null;
+        }
+        else if (mode == DequeueMode.Highest)
+        {
+            Node best = head;
+            Node current = head;
+            while (current != null)
+            {
+                if (current.Priority > best.Priority)
+                    best = current;
+                current = current.Next;
+            }
+            RemoveNode(best);
+            return best;
+        }
+        else if (mode == DequeueMode.Lowest)
+        {
+            Node lowest = head;
+            Node current = head;
+            while (current != null)
+            {
+                if (current.Priority > lowest.Priority)
+                    lowest = current;
+                current = current.Next;
+            }
+            RemoveNode(lowest);
+            return lowest;
+        }
+        else if (mode == DequeueMode.Oldest)
+        {
+            Node oldest = head;
+            Node current = head;
+            while (current != null)
+            {
+                if (current.InsertionOrder < oldest.InsertionOrder)
+                    oldest = current;
+                current = current.Next;
+            }
+            RemoveNode(oldest);
+            return oldest;
+        }
+        else if (mode == DequeueMode.Newest)
+        {
+            Node newest = head;
+            Node current = head;
+            while (current != null)
+            {
+                if (current.InsertionOrder > newest.InsertionOrder)
+                    newest = current;
+                current = current.Next;
+            }
+            RemoveNode(newest);
+            return newest;
+        }
+        return null;
     }
 }
