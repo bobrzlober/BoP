332ebe58a06bd83c0091a38a9a5da45c4f316c45
bobr_zlober <bobr.zlober@gmail.com>
2026-04-15 15:33:14 +0300

    started lab_5

---
+++ BP_Lab_5/Lab_5.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+static List<TResult> Map<T, TResult>(
+    IEnumerable<T> source, Func<T, TResult> transform)
+{
+    var result = new List<TResult>();
+    foreach(var item in source)
+    {
+        result.Add(transform(item));
+    }
+    return result;
+}
+var numbers = new List<int> {1,2,3,4,5};
+var doubled = Map(numbers, x=> x*2);
+Console.WriteLine(string.Join(", ", doubled));
+var asString = Map(numbers, x => $"num_{x}");
+Console.WriteLine(string.Join(", ", asString));
