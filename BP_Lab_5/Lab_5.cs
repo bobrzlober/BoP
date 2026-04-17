c8c49e012e90bc428a9df149b8c81e4a091dd80f
bobr_zlober <bobr.zlober@gmail.com>
2026-04-17 11:34:22 +0300

    tryng to use async

--- BP_Lab_5/Lab_5.cs
+++ BP_Lab_5/Lab_5.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
-static List<TResult> Map<T, TResult>(
-    IEnumerable<T> source, Func<T, TResult> transform)
-{
+static async Task<List<TResult>> MapAsync<T, TResult>(
+    IEnumerable<T> source,
+    Func<T, Task<TResult>> transform)
+{   
     var result = new List<TResult>();
-    foreach(var item in source)
+    foreach (var item in source)
     {
-        result.Add(transform(item));
+        TResult transformed = await transform(item);
+        result.Add(transformed);
     }
+
     return result;
 }
-var numbers = new List<int> {1,2,3,4,5};
-var doubled = Map(numbers, x=> x*2);
+var numbers = new List<int> { 1, 2, 3, 4, 5 };
+async Task<int> SlowDouble(int x)
+{
+    await Task.Delay(500);
+    return x * 2;
+}
+var doubled = await MapAsync(numbers, SlowDouble);
 Console.WriteLine(string.Join(", ", doubled));
-var asString = Map(numbers, x => $"num_{x}");
-Console.WriteLine(string.Join(", ", asString));
