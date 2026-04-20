ce0d7b833b16bd0d8d3e02e34e75137fe50b9e8a
bobr_zlober <bobr.zlober@gmail.com>
2026-04-20 19:18:53 +0300

    finished Lab5, going to make the Demo part separated

--- BP_Lab_5/Lab_5.cs
+++ BP_Lab_5/Lab_5.cs
@@ -50,6 +50,20 @@ static void MapAsyncCallback<T, TResult>(
         }
     });
 }
+static async Task<List<TResult>> MapAsyncCancellable<T, TResult>(
+    IEnumerable<T> source,
+    Func<T, Task<TResult>> transform,
+    CancellationToken cancellationToken)
+{
+    var result = new List<TResult>();
+    foreach(var item in source)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        TResult transformed = await transform(item);
+        result.Add(transformed);
+    }
+    return result;
+}
 var numbers = new List<int> { 1, 2, 3, 4, 5 };
 async Task<int> SlowDouble(int x)
 {
@@ -57,12 +71,29 @@ async Task<int> SlowDouble(int x)
     return x * 2;
 }
 var sw = System.Diagnostics.Stopwatch.StartNew();
+
 var seqenced = await MapAsync(numbers, SlowDouble);
 Console.WriteLine($"Seqenced: {sw.ElapsedMilliseconds} ms");
+
 sw.Restart();
+
 var parallel = await MapAsyncParallel(numbers, SlowDouble);
 Console.WriteLine($"Parallel: {sw.ElapsedMilliseconds} ms");
+
 sw.Restart();
+
 MapAsyncCallback(numbers, SlowDouble, OnComplete => Console.WriteLine($"Ready: {string.Join(",", OnComplete)}"), OnError => Console.WriteLine($"Error: {OnError.Message}"));
 Console.WriteLine("this line should appear before the result");
 await Task.Delay(5000);
+
+var cts = new CancellationTokenSource();
+cts.CancelAfter(TimeSpan.FromSeconds(2));
+try
+{
+    var cancelled = await MapAsyncCancellable(numbers, SlowDouble, cts.Token);
+    Console.WriteLine($"Cancelation token result: {string.Join(", ", cancelled)}");
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Canceled");
+}
