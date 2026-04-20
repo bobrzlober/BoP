52051b84fe077248ab469af9f675a5967e38d1ff
bobr_zlober <bobr.zlober@gmail.com>
2026-04-20 18:22:13 +0300

    updated lab5

--- BP_Lab_5/Lab_5.cs
+++ BP_Lab_5/Lab_5.cs
@@ -1,19 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+static async Task<List<TResult>> MapAsync <T, TResult>(
+    IEnumerable<T> source,
+    Func<T, Task<TResult>> transform)
+{
+    var result = new List<TResult>();
+    foreach(var item in source)
+    {
+        TResult transformed = await transform(item);
+        result.Add(transformed); 
+    }
+    return result;
+}
 
-static async Task<List<TResult>> MapAsync<T, TResult>(
+static async Task<List<TResult>> MapAsyncParallel<T, TResult>(
     IEnumerable<T> source,
     Func<T, Task<TResult>> transform)
 {   
-    var result = new List<TResult>();
-    foreach (var item in source)
+    List<Task<TResult>> tasks = new List<Task<TResult>>();
+        foreach(var item in source)
     {
-        TResult transformed = await transform(item);
-        result.Add(transformed);
+        tasks.Add(transform(item));
     }
-
-    return result;
+    TResult[] results = await Task.WhenAll(tasks);
+    return new List<TResult>(results); 
+}
+static void MapAsyncCallback<T, TResult>(
+    IEnumerable<T> source,
+    Func<T, Task<TResult>> transform,
+    Action<List<TResult>> OnComplete,
+    Action<Exception>? OnError = null)
+{
+    Task.Run(async () =>
+    {
+        try
+        {
+            var result = new List<TResult>();
+            foreach(var item in source)
+            {
+                TResult transformed = await transform(item);
+                result.Add(transformed);
+            }
+            OnComplete(result);
+        }
+        catch (Exception exc)
+        {
+            OnError?.Invoke(exc);
+        }
+    });
 }
 var numbers = new List<int> { 1, 2, 3, 4, 5 };
 async Task<int> SlowDouble(int x)
@@ -21,5 +56,13 @@ async Task<int> SlowDouble(int x)
     await Task.Delay(500);
     return x * 2;
 }
-var doubled = await MapAsync(numbers, SlowDouble);
-Console.WriteLine(string.Join(", ", doubled));
+var sw = System.Diagnostics.Stopwatch.StartNew();
+var seqenced = await MapAsync(numbers, SlowDouble);
+Console.WriteLine($"Seqenced: {sw.ElapsedMilliseconds} ms");
+sw.Restart();
+var parallel = await MapAsyncParallel(numbers, SlowDouble);
+Console.WriteLine($"Parallel: {sw.ElapsedMilliseconds} ms");
+sw.Restart();
+MapAsyncCallback(numbers, SlowDouble, OnComplete => Console.WriteLine($"Ready: {string.Join(",", OnComplete)}"), OnError => Console.WriteLine($"Error: {OnError.Message}"));
+Console.WriteLine("this line should appear before the result");
+await Task.Delay(5000);
